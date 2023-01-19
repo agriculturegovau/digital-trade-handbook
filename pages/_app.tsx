9b54419fc3290563edad431b6af47882401caa87
Jordan Overbye <jordanoverbye@gmail.com>
2023-01-19 13:44:42 +1000

    add google analytics
    remove console.log
    
    space

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,25 @@
 import type { AppProps } from 'next/app';
+import { Fragment, useEffect } from 'react';
+import Script from 'next/script';
+import { useRouter } from 'next/router';
 import Head from 'next/head';
 import { Core } from '@ag.ds-next/react/core';
 import { theme } from '@ag.ds-next/react/ag-branding';
 import { LinkComponent } from '../components/LinkComponent';
+import { trackPageView, GA_MEASUREMENT_ID } from '../lib/gtag';
 
 export default function App({ Component, pageProps }: AppProps) {
+	const router = useRouter();
+
+	useEffect(() => {
+		router.events.on('routeChangeComplete', trackPageView);
+		router.events.on('hashChangeComplete', trackPageView);
+		return () => {
+			router.events.off('routeChangeComplete', trackPageView);
+			router.events.off('hashChangeComplete', trackPageView);
+		};
+	}, [router.events]);
+
 	return (
 		<Core theme={theme} linkComponent={LinkComponent}>
 			<Head>
@@ -28,6 +43,29 @@ export default function App({ Component, pageProps }: AppProps) {
 					href="/digital-trade-handbook/favicon/manifest.webmanifest"
 				/>
 			</Head>
+			{/* Global Site Tag (gtag.js) - Google Analytics */}
+			{GA_MEASUREMENT_ID ? (
+				<Fragment>
+					<Script
+						strategy="afterInteractive"
+						src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+					/>
+					<Script
+						id="gtag-init"
+						strategy="afterInteractive"
+						dangerouslySetInnerHTML={{
+							__html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+						}}
+					/>
+				</Fragment>
+			) : null}
 			<Component {...pageProps} />
 		</Core>
 	);
